4d91f85517e2c78bdec2596133f35ae3e7a677ef
Ernesto Cárdenas <ernesto.cardenas@gmail.com>
2020-05-17 15:59:52 -0500

    actualizando el body json

--- api/MensajeBasico/index.js
+++ api/MensajeBasico/index.js
@@ -8,6 +8,8 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: { 
+            text: responseMessage
+          }
     };
 }
