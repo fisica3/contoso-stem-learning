dd4bad7a8dc35762731d2b30c2a2bdae807ea72d
Ernesto Cárdenas <ernesto.cardenas@gmail.com>
2021-05-21 23:28:38 -0500

    Update index.js

--- api/GetJamStem/index.js
+++ api/GetJamStem/index.js
@@ -13,7 +13,7 @@ module.exports = async function (context, req) {
                     "name": "Science",
                     "description": "Learn the fundamentals for what makes our world spin.",
                     "url": "https://en.wikipedia.org/wiki/Science",
-                    "image": "https://dgdemoassets.blob.core.windows.net/demoimages/science.jpg?sp=rl&st=2020-04-28T07:08:43Z&se=2021-04-29T07:08:00Z&sv=2019-10-10&sr=b&sig=6WrtgoRgLyPfbVvyXqTkTRoF4oWkp1fVNlijvl%2BdxFs%3D"
+                    "image": "/images/science.jpg"
                 },
                 {
                     "name": "Technology",
@@ -25,13 +25,13 @@ module.exports = async function (context, req) {
                     "name": "Engineering",
                     "description": "Learn how to make things that endure the test of time.",
                     "url": "https://en.wikipedia.org/wiki/Engineering",
-                    "image": "https://dgdemoassets.blob.core.windows.net/demoimages/engineering.jpg?sp=rl&st=2020-04-28T07:06:11Z&se=2021-04-29T07:06:00Z&sv=2019-10-10&sr=b&sig=3ml4rzUBjhAc95v1mVdqCUjHrdQDnrWGFqIhh6BYAYE%3D"
+                    "image": "/images/engineering.jpg"
                 },
                 {
                     "name": "Math",
                     "description": "Learn about the numbers that power our mesmerizing world.",
                     "url": "https://en.wikipedia.org/wiki/Mathematics",
-                    "image": "https://dgdemoassets.blob.core.windows.net/demoimages/math.jpg?sp=rl&st=2020-04-28T07:07:23Z&se=2021-04-29T07:07:00Z&sv=2019-10-10&sr=b&sig=Ak4CNN3ayp1KEtUrgRxmooWqOe1vTrXlml4qGEf2m58%3D"
+                    "image": "/images/math.jpg"
                 }
 
             ]
